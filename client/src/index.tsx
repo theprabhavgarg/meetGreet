4bab66b229df7de8e07fd3367ad4565c8d0ea2be
PRABHAV GARG <prabhavg@PRABHAVs-MacBook-Pro.local>
2025-12-12 18:35:08 +0530

    changes related to netlify

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './config/api'; // Configure axios with base URL
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
